503ca4a462f5933487b77906ab3ab5311a030576
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-12-01 22:29:56 +0530

    added headings parser

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,111 @@
 fn main() {
-    let md = "# Heading 1\n## Heading 2";
-    println!("[debug] {md}");
-    let mut html = String::from("");
-    let mut h1 = String::from("");
-    let mut h2 = String::from("");
-    let mut heading_count = 0;
+    let md = String::from("# Heading 1\n#### Heading 4\n## Heading 2");
 
-    let mut tag_h1 = false;
-    let mut tag_h2 = false;
+    let mut count_hash = 0;
+    let mut flag_h1 = false;
+    let mut flag_h2 = false;
+    let mut flag_h3 = false;
+    let mut flag_h4 = false;
+    let mut flag_h5 = false;
+    let mut flag_h6 = false;
+    let mut text_h1 = String::from("");
+    let mut text_h2 = String::from("");
+    let mut text_h3 = String::from("");
+    let mut text_h4 = String::from("");
+    let mut text_h5 = String::from("");
+    let mut text_h6 = String::from("");
 
     for i in md.chars() {
-        if tag_h1 {
-            h1.push(i);
+        print!("|");
+        print!("{i}");
+
+        // ---------------- headings -------------------
+        if flag_h1 {
+            if i == '\n' {
+                flag_h1 = false;
+            } else {
+                text_h1.push(i);
+            }
         }
-        if tag_h2 {
-            h2.push(i);
+        if flag_h2 {
+            if i == '\n' {
+                flag_h2 = false;
+            } else {
+                text_h2.push(i);
+            }
         }
-        if heading_count == 2 && i != '#' {
-            tag_h2 = true;
+        if flag_h3 {
+            if i == '\n' {
+                flag_h3 = false;
+            } else {
+                text_h3.push(i);
+            }
         }
-        if heading_count == 1 && i != '#' {
-            tag_h1 = true;
+        if flag_h4 {
+            if i == '\n' {
+                flag_h4 = false;
+            } else {
+                text_h4.push(i);
+            }
         }
-        if heading_count > 0 && i == '#' {
-            heading_count += 1;
+        if flag_h5 {
+            if i == '\n' {
+                flag_h5 = false;
+            } else {
+                text_h5.push(i);
+            }
+        }
+        if flag_h6 {
+            if i == '\n' {
+                flag_h6 = false;
+            } else {
+                text_h6.push(i);
+            }
         }
         if i == '#' {
-            heading_count = 0;
-            tag_h1 = false;
-            tag_h2 = false;
-            heading_count += 1;
+            count_hash += 1;
         }
+        if count_hash > 0 && i == ' ' {
+            print!("({count_hash})");
+            if count_hash == 1 {
+                flag_h1 = true;
+            }
+            if count_hash == 2 {
+                flag_h2 = true;
+            }
+            if count_hash == 3 {
+                flag_h3 = true;
+            }
+            if count_hash == 4 {
+                flag_h4 = true;
+            }
+            if count_hash == 5 {
+                flag_h5 = true;
+            }
+            if count_hash == 6 {
+                flag_h6 = true;
+            }
+            count_hash = 0;
+        }
+        if count_hash == 2 && i != '#' {
+            println!("{i}");
+            break;
+        }
+        // ---------------- headings -------------------
+
+        print!("|");
     }
 
-    for i in h1.chars() {
-        println!("[debug] {i}");
-    }
-
-    // for i in html.chars() {
-    //     println!("[debug] {i}");
-    // }
+    let tag_h1 = format!("<h1>{text_h1}</h1>");
+    let tag_h2 = format!("<h2>{text_h2}</h2>");
+    let tag_h3 = format!("<h3>{text_h3}</h3>");
+    let tag_h4 = format!("<h4>{text_h4}</h4>");
+    let tag_h5 = format!("<h5>{text_h5}</h5>");
+    let tag_h6 = format!("<h6>{text_h6}</h6>");
+    println!("\nh1 - {}", tag_h1);
+    println!("h2 - {}", tag_h2);
+    println!("h3 - {}", tag_h3);
+    println!("h4 - {}", tag_h4);
+    println!("h5 - {}", tag_h5);
+    println!("h6 - {}", tag_h6);
 }
